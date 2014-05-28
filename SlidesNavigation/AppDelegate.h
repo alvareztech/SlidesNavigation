b243576c7bdc00f90161ae93983cc63ed2e3ef49
Daniel Alvarez <i@danyalvarez.com>
2014-05-28 15:00:57 -0400

    Initial Commit

---
+++ SlidesNavigation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlidesNavigation
+//
+//  Created by Daniel Alvarez on 5/28/14.
+//  Copyright (c) 2014 Daniel Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlidesNavigation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SlidesNavigation
+//
+//  Created by Daniel Alvarez on 5/28/14.
+//  Copyright (c) 2014 Daniel Alvarez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
